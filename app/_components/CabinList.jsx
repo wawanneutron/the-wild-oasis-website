34ec25bf8c4c59df1627a4bc6e8ca00f116feb17
wawanneutron <wawanneutron1331@gmail.com>
2025-07-27 08:08:35 +0700

    feat: cabin list component

---
+++ app/_components/CabinList.jsx
@@ -0,0 +1,19 @@
+import { getCabins } from '@/app/_lib/data-service'
+import CabinCard from './CabinCard'
+
+async function CabinList() {
+  const cabins = await getCabins()
+  console.log(cabins)
+
+  if (!cabins.length) return null
+
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+      {cabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  )
+}
+
+export default CabinList
