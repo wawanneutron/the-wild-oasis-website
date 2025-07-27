64dde1ae7bac62ee35476658f08015294cd4370b
wawanneutron <wawanneutron1331@gmail.com>
2025-07-27 08:07:17 +0700

    feat: meta about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 import image1 from '@/public/about-1.jpg'
 
+export const metadata = {
+  title: 'About'
+}
+
 export default function Page() {
   return (
     <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
