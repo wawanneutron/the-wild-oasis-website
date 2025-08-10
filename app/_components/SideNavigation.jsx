429bbe34878bb97c3ccaf931fb5f60111bd91739
wawanneutron <wawanneutron1331@gmail.com>
2025-08-10 13:08:33 +0700

    feat: styling active sidebar

--- app/_components/SideNavigation.jsx
+++ app/_components/SideNavigation.jsx
@@ -1,6 +1,9 @@
+'use client'
+
 import { CalendarDaysIcon, HomeIcon, UserIcon } from '@heroicons/react/24/solid'
 import SignOutButton from '@/app/_components/SignOutButton'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const navLinks = [
   {
@@ -21,13 +24,18 @@ const navLinks = [
 ]
 
 function SideNavigation() {
+  const pathName = usePathname()
+
+  console.log(pathName)
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
+                pathName === link.href ? 'bg-primary-900' : ''
+              }`}
               href={link.href}
             >
               {link.icon}
