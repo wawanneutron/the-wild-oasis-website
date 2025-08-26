3da53059bb0361ebecaefcee23ea70f554fada7d
wawanneutron <wawanneutron1331@gmail.com>
2025-08-26 22:39:48 +0700

    feat: update guest - action server

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,8 +1,36 @@
 'use server'
 
-import { signIn, signOut } from '@/app/_lib/auth'
+import { auth, signIn, signOut } from '@/app/_lib/auth'
+import { supabase } from './supabase'
+import { revalidatePath } from 'next/cache'
 
 export const signInAction = async () =>
   await signIn('google', { redirectTo: '/account' })
 
 export const signOutAction = async () => await signOut({ redirectTo: '/' })
+
+export const updateGuest = async (formData) => {
+  const session = await auth()
+  if (!session) throw new Error('You must be logged in to update your profile.')
+
+  const nationalID = formData.get('nationalID')
+  const [nationality, countryFlag] = formData.get('nationality').split('%')
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error('Please provide a valid national ID')
+
+  const updateData = { nationalID, nationality, countryFlag }
+
+  console.log('serveraction update guest', updateData)
+
+  const { data, error } = await supabase
+    .from('guests')
+    .update(updateData)
+    .eq('id', session.user.guestId)
+    .select()
+    .single()
+
+  if (error) throw new Error('Guest could not be updated')
+
+  revalidatePath('/account/profile')
+}
