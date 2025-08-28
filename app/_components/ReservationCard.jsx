3151957de4c4db50686405908f71580fe4231b76
wawanneutron <wawanneutron1331@gmail.com>
2025-08-28 20:13:40 +0700

    feat: validation delete

--- app/_components/ReservationCard.jsx
+++ app/_components/ReservationCard.jsx
@@ -69,16 +69,18 @@ function ReservationCard({ booking }) {
         </div>
       </div>
 
-      <div className="flex flex-col border-l border-primary-800 w-[100px]">
-        <a
-          href={`/account/reservations/edit/${id}`}
-          className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
-        >
-          <PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-          <span className="mt-1">Edit</span>
-        </a>
-        <DeleteReservation bookingId={id} />
-      </div>
+      {!isPast(new Date(startDate)) && (
+        <div className="flex flex-col border-l border-primary-800 w-[100px]">
+          <a
+            href={`/account/reservations/edit/${id}`}
+            className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+          >
+            <PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+            <span className="mt-1">Edit</span>
+          </a>
+          <DeleteReservation bookingId={id} />
+        </div>
+      )}
     </div>
   )
 }
