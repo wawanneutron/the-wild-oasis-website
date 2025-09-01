153926f83828ab816b2783282b6cee38411fff0d
wawanneutron <wawanneutron1331@gmail.com>
2025-09-01 11:35:50 +0700

    feat: dashboard guest reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { auth, signIn, signOut } from '@/app/_lib/auth'
 import { supabase } from './supabase'
 import { revalidatePath } from 'next/cache'
 import { getBookings } from './data-service'
+import { redirect } from 'next/navigation'
 
 export const signInAction = async () =>
   await signIn('google', { redirectTo: '/account' })
@@ -40,7 +41,32 @@ export const updateGuest = async (formData) => {
   revalidatePath('/account/profile')
 }
 
-export const deleteReservation = async (bookingId) => {
+export const updateBooking = async (formData) => {
+  const session = await auth()
+  if (!session)
+    throw new Error('You must be logged in to update your reservation.')
+
+  const bookingId = parseInt(formData.get('bookingId'))
+  const numGuests = parseInt(formData.get('numGuests'))
+  const observations = formData.get('observations') || null
+
+  const updateData = { numGuests, observations }
+
+  console.log(updateData)
+
+  const { data, error } = await supabase
+    .from('bookings')
+    .update(updateData)
+    .eq('id', bookingId)
+    .select()
+    .single()
+
+  if (error) throw new Error('Reservation could not be updated')
+
+  redirect('/account/reservations')
+}
+
+export const deleteBooking = async (bookingId) => {
   const session = await auth()
   if (!session) throw new Error('You must be logged in to update your profile.')
 
