78eab40b69db10d3449ae58df85e930604c0cce2
wawanneutron <wawanneutron1331@gmail.com>
2025-10-20 00:19:11 +0700

    feat: date selector

--- app/_components/DateSelector.jsx
+++ app/_components/DateSelector.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { isWithinInterval } from 'date-fns'
+import { differenceInDays, isPast, isSameDay, isWithinInterval } from 'date-fns'
 import { DayPicker } from 'react-day-picker'
 import 'react-day-picker/dist/style.css'
 import { useReservation } from './ReservationContext'
@@ -18,13 +18,12 @@ function isAlreadyBooked(range, datesArr) {
 function DateSelector({ cabin, settings, bookedDates }) {
   const { range, setRange, resetRange } = useReservation()
 
-  // CHANGE
-  const regularPrice = 23
-  const discount = 23
-  const numNights = 23
-  const cabinPrice = 23
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range
 
-  // SETTINGS
+  const { regularPrice, discount } = cabin
+  const numNights = differenceInDays(range.to, range.from)
+  const cabinPrice = numNights * (regularPrice - discount)
+
   const { minBookingLength, maxBookingLength } = settings
 
   const handleSelect = (selectedRange) => {
@@ -40,7 +39,7 @@ function DateSelector({ cabin, settings, bookedDates }) {
         min={minBookingLength}
         max={maxBookingLength}
         onSelect={handleSelect} // Write the state if selected a date
-        selected={range} // Read the state
+        selected={displayRange} // Read the state
         // // OLD API
         // fromMonth={new Date()}
         // fromDate={new Date()}
@@ -51,6 +50,10 @@ function DateSelector({ cabin, settings, bookedDates }) {
         endMonth={new Date(new Date().getFullYear() + 5, 11)} // December of the year 5 years from now
         captionLayout="dropdown"
         numberOfMonths={2} // Show only 2 months
+        disabled={(curDate) =>
+          isPast(curDate) ||
+          bookedDates.some((date) => isSameDay(date, curDate))
+        }
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
